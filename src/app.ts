26c1f245496c2bd3755d71d7704fcd0c3a3ae5a9
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-07-11 23:47:15 +0600

    required directories and files added

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+import cors from "cors";
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("sport zone");
+});
+export default app;
