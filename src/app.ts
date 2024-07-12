e41fdbe076e3394ac82fc00206a4c751df9e2116
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-07-12 12:59:51 +0600

    cors related issue resolved

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import productRoutes from "./app/modules/products/product.route";
 
 // parsers
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5313", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // routes
 app.use("/api/v1/products", productRoutes);
